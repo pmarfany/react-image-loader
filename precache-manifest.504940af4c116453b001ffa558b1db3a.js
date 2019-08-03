c9f5772fca2ced4909f902316fdd5928c648421f
Pau Marfany <p.marfany_96@hotmail.com>
2019-08-03 20:03:02 +0200

    Updates

---
+++ precache-manifest.504940af4c116453b001ffa558b1db3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0cc5ea1dccb8ecebe25820c5f795f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10beec984336545a825f",
+    "url": "/static/css/main.e2c04713.chunk.css"
+  },
+  {
+    "revision": "d8c8a2420f963db28ff7",
+    "url": "/static/js/2.2692ef18.chunk.js"
+  },
+  {
+    "revision": "10beec984336545a825f",
+    "url": "/static/js/main.37e1a400.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86f490107f3281d27e6d5e5437d1f2d5",
+    "url": "/static/media/add.86f49010.svg"
+  },
+  {
+    "revision": "fd8612a0b21c983f198ce034c5801f40",
+    "url": "/static/media/close.fd8612a0.svg"
+  },
+  {
+    "revision": "ecce6c6f75a125b6bc14b1cc241e8577",
+    "url": "/static/media/edit.ecce6c6f.svg"
+  },
+  {
+    "revision": "71882ff72133ac79b28fae75c452d1de",
+    "url": "/static/media/picture.71882ff7.svg"
+  }
+]);
