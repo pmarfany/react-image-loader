c0bada0c0821c5f940c367b3054807e0b7c1168a
Pau Marfany <p.marfany_96@hotmail.com>
2019-08-03 20:05:24 +0200

    Updates

---
+++ precache-manifest.da7133bc0023f04c4868a6ea6dad64a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5f5437f2bd940b4cb748c156158dc3",
+    "url": "/react-image-loader/index.html"
+  },
+  {
+    "revision": "3035a46cf00903fc2192",
+    "url": "/react-image-loader/static/css/main.e2c04713.chunk.css"
+  },
+  {
+    "revision": "c9b955312141bdde08a5",
+    "url": "/react-image-loader/static/js/2.d684a915.chunk.js"
+  },
+  {
+    "revision": "3035a46cf00903fc2192",
+    "url": "/react-image-loader/static/js/main.96be0204.chunk.js"
+  },
+  {
+    "revision": "56be0aedf33f7286ec41",
+    "url": "/react-image-loader/static/js/runtime~main.cf9ce815.js"
+  },
+  {
+    "revision": "86f490107f3281d27e6d5e5437d1f2d5",
+    "url": "/react-image-loader/static/media/add.86f49010.svg"
+  },
+  {
+    "revision": "fd8612a0b21c983f198ce034c5801f40",
+    "url": "/react-image-loader/static/media/close.fd8612a0.svg"
+  },
+  {
+    "revision": "ecce6c6f75a125b6bc14b1cc241e8577",
+    "url": "/react-image-loader/static/media/edit.ecce6c6f.svg"
+  },
+  {
+    "revision": "71882ff72133ac79b28fae75c452d1de",
+    "url": "/react-image-loader/static/media/picture.71882ff7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.504940af4c116453b001ffa558b1db3a.js"
+  "/react-image-loader/precache-manifest.da7133bc0023f04c4868a6ea6dad64a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-image-loader/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
